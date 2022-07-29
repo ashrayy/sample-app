37227698ae8e4f3791138f1dc9725357d91ffac6
ashrayy <trivediashray786@gmail.com>
2022-07-29 12:15:09 +0530

    Arrays/Slicing/Sorting

--- arrays.go
+++ arrays.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
@@ -16,4 +17,28 @@ func main() {
 	numArr[4] =6
 	fmt.Println(numArr)
 	fmt.Println(len(numArr))
+
+	var sliceableArr = []string{"Orange","Banana","Apple"}
+	sliceableArr = append(sliceableArr,"Watermelon")
+	fmt.Println(sliceableArr)
+	fmt.Println(len(sliceableArr))
+
+	sliceableArr = append(sliceableArr[1:len(sliceableArr)])
+	fmt.Println(sliceableArr)
+	fmt.Println(len(sliceableArr))
+
+	sliceableArr = append(sliceableArr[:len(sliceableArr)-1])
+	fmt.Println(sliceableArr)
+	fmt.Println(len(sliceableArr))
+
+	var numberArr = make([]int, 5)
+	numberArr[0] = 12
+	numberArr[1] = 243
+    numberArr[2] = 886
+	numberArr[3] = 7686
+	numberArr[4] = 356
+    fmt.Println(numberArr)
+
+	sort.Ints(numberArr)
+	fmt.Println(numberArr)
 }
